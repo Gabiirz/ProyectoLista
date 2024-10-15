72248dadaf472907c9fb333655cd9b99d18ce317
Gabiirz <gabriel.rezende303@gmail.com>
2024-10-15 19:14:48 +0200

    header-main1

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent{
+  constructor(private router: Router){}
 
+  
+
+
+  navigateTo(route: string){
+    this.router.navigate([route]);
+  }
 }
